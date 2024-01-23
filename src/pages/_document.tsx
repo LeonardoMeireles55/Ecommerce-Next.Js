f3630677fe3eb1e5b690fcef3cf58d62940ab66b
leonardo <leomeireles55@hotmail.com>
2024-01-23 12:07:39 -0300

    logo fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
-      <link rel="icon" href="./icon.png" sizes="any" />
+      <link rel="icon" href="./FOOTER-LOGO.png" sizes="any" />
       <title>GIOM</title>
       <body className="bg-yellow-100 bg-opacity-25 font-mono">
         <Main />
