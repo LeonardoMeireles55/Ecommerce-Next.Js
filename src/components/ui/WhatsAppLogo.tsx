6d56f2d21177ba0e6b2b77092fa65533fdfc0611
leonardo <leomeireles55@hotmail.com>
2024-03-22 18:01:55 -0300

    Update SVG fill color and WhatsApp logo background color

--- src/components/ui/WhatsAppLogo.tsx
+++ src/components/ui/WhatsAppLogo.tsx
@@ -9,8 +9,9 @@ export default function WhatsAppLogo() {
     };
 
     return (
-        <span className='w-10 h-10 md:w-12 md:h-12 flex items-center justify-center bg-orange-100 rounded-full shadow-xl fixed bottom-6 right-6'>
-                <Image onClick={handleClick} className='w-6 md:w-8 opacity-100 hover:cursor-pointer'
+
+        <span className='w-10 h-10 md:w-12 md:h-12 flex items-center justify-center bg-[#25D366] rounded-full shadow-xl fixed bottom-6 right-6'>
+                <Image onClick={handleClick} className='w-6 md:w-8 opacity-75 hover:cursor-pointer'
                     src="/whats2.svg"
                     width={100}
                     height={100}
