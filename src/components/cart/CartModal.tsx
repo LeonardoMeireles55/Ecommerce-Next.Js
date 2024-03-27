de63a685a47a0c0fbd0d3dd12958e3e3edfe11c5
leonardo <leomeireles55@hotmail.com>
2024-03-27 19:22:19 -0300

    Update Navbar and CartModal styles

--- src/components/cart/CartModal.tsx
+++ src/components/cart/CartModal.tsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
 import Link from "next/link";
 import CartContext from "../context/CartContext";
-import PricePerProduct from "./PricePerProduct";
 
 
 const CartModal = () => {
@@ -12,8 +11,8 @@ const CartModal = () => {
         <div className="bg-orange-50 w-full md:w-80 rounded-md p-6 md:p-10 shadow-xl">
             <ul className="space-y-2">
                 {cart.map((product: any) => (
-                    <li key={product.id} className="flex items-center space-x-2 md:space-x-4">
-                        <img className="w-12 h-12 md:w-20 md:h-20" src={product.photoLink} alt="Imagem do produto" />
+                    <li key={product.id} className="flex items-center space-y-2 space-x-2 md:space-x-4">
+                        <img className="w-10 h-12 md:w-20 md:h-20 rounded-md" src={product.photoLink} alt="Imagem do produto" />
                         <div>
                             <h5 className="text-xs md:text-sm font-semibold">{product.name}</h5>
                             <h6 className="text-xs md:text-sm text-black">{(product.price - (product.price * product.offPrice / 100)).toFixed(2)} <del className="text-gray-500">{product.price}</del></h6>
@@ -25,7 +24,7 @@ const CartModal = () => {
             <div className="flex flex-col md:flex-row justify-center items-center mt-4 md:mt-6 gap-1 md:gap-4">
                 <button onClick={removeAllProducts} className="text-xs justify-center items-center md:text-sm text-orange-400 focus:outline-none">Esvaziar</button>
                 <Link href={"/cart"}>
-                    <button className="text-xs md:text-sm px-3 md:px-4 py-1.5 md:py-2 bg-orange-400 text-white rounded-md hover:bg-orange-500 focus:outline-none transition duration-300 ease-in-out">Ver Carrinho</button>
+                    <button className="text-xs md:text-sm px-3 md:px-4 py-1.5 md:py-2 bg-orange-300 text-white rounded-md hover:bg-orange-500 focus:outline-none transition duration-300 ease-in-out">Ver Carrinho</button>
                 </Link>
             </div>
         </div>
