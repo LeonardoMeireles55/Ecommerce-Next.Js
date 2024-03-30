e6e1ab3d4db21f2451c4d9fbbb3ebf64783381e3
leonardo <leomeireles55@hotmail.com>
2024-03-30 10:40:26 -0300

    Refactor product card styling

--- src/components/product/ProductsCards.tsx
+++ src/components/product/ProductsCards.tsx
@@ -22,7 +22,7 @@ const ProductsCards = ({ categoryProps }: CategoryProps): JSX.Element => {
     const discountedPrice = (price - (price * offPrice) / 100).toFixed(2);
 
     return (
-      <div id="products-cards" key={id} className="relative p-6 md:px-8 flex flex-col items-center justify-center text-center border border-dashed border-orange-200 border-opacity-75 border-1 shadow-sm hover:scale-105">
+      <div id="products-cards" key={id} className="relative p-6 md:px-8 flex flex-col items-center justify-center text-center border border-dashed border-orange-200 border-opacity-75 border-1 shadow-sm">
         <Link href={'/product-description?id=' + product.id}>
           <div>
             {loading && <ProgressSpinner style={{ width: '25px', height: '25px' }} strokeWidth="4" fill="transparent" animationDuration=".5s" />}
